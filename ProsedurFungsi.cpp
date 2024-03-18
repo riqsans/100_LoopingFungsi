88dd0cfe06a7f870acc7fc6232ae17620b2ccffc
riqsans <thrq88@gmail.com>
2024-03-18 16:01:37 +0700

    ProsedurFungsi

---
+++ ProsedurFungsi.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+string nama;
+int nXpander, nPorsche, nAvanza, nBrio, nLamborghini;
+int hXpander = 300, hPorsche = 8000, hAvanza = 250, hBrio = 200, hLamborghini = 6000;
+
+void input(){
+    cout << "Nama Pembeli :";
+    cin >> nama;
+    cout << "Jumlah Xpander :";
+    cin >> nXpander;
+
+    cout << "Jumlah Porsche :";
+    cin >> nPorsche;
+
+    cout << "Jumlah Avanza :";
+    cin >> nAvanza;
+    
+    cout << "Jumlah Brio :";
+    cin >> nBrio;
+
+    cout << "Jumlah Lamborghini :";
+    cin >> nLamborghini;
+}
+
+int TotalHarga(){
+    return (nXpander * hXpander) + (nPorsche * hPorsche) + (nAvanza * hAvanza) + (nBrio * hBrio) + (nLamborghini * hLamborghini);
+}
+
+void display(){
+    cout << "Total harganya :" << TotalHarga() << endl;
+}
+
+int main(){
+    char  pilihan;
+    do{
+    input();
+    display();
+    cout << "Apakah ingin membeli lagi ?";
+    cin >> pilihan;
+    }while(pilihan == 'y' || pilihan == 'Y');
+}
